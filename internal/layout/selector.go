38e5599b08d1078900b6e032808f353b2a9a10f4
antklim <antklim@gmail.com>
2021-06-10 08:20:56 +1000

    Layout selector WIP

--- internal/layout/selector.go
+++ internal/layout/selector.go
@@ -1,23 +1,40 @@
 package layout
 
+import "errors"
+
 type Category string
 
+func (c Category) isUnknown() bool {
+	return c != CategoryService
+}
+
 const (
-	// CategoryUnknown represents unknown category of a layout.
-	CategoryUnknown Category = "unknown"
 	// CategoryService represents service category of a layout.
 	CategoryService Category = "srv"
 )
 
+var (
+	errUnknownCategory = errors.New("unknown layout category")
+	errUnknownServer   = errors.New("unknown server")
+)
+
 type Server string
 
+func (s Server) isUnknown() bool {
+	return s != ServerHTTP
+}
+
 const (
-	// ServerUnknown represents unknown
-	ServerUnknown Server = "unknown"
 	// ServerHTTP represents http server
 	ServerHTTP Server = "http"
 )
 
-func Selector(category, server string) ([]Node, error) {
+func Selector(c Category, s Server) ([]Node, error) {
+	if c.isUnknown() {
+		return nil, errUnknownCategory
+	}
+	if s.isUnknown() {
+		return nil, errUnknownServer
+	}
 	return nil, nil
 }
--- internal/layout/selector_test.go
+++ internal/layout/selector_test.go
@@ -1,10 +1,17 @@
-package layout
+package layout_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/antklim/chef/internal/layout"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestLayoutSelector(t *testing.T) {
 	t.Run("returns unknown category error when unknown category provided", func(t *testing.T) {
-		t.Skip("not implemented")
+		nodes, err := layout.Selector("", "")
+		assert.EqualError(t, err, "unknown layout category")
+		assert.Nil(t, nodes)
 	})
 
 	t.Run("returns default service layout for service category", func(t *testing.T) {
@@ -16,6 +23,8 @@ func TestLayoutSelector(t *testing.T) {
 	})
 
 	t.Run("returns unknown server error for service category and unknown server", func(t *testing.T) {
-		t.Skip("not implemented")
+		nodes, err := layout.Selector(layout.CategoryService, "")
+		assert.EqualError(t, err, "unknown server")
+		assert.Nil(t, nodes)
 	})
 }
