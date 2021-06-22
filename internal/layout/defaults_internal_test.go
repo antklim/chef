c83bed1453acbb18d6c73aa027fdecfabd2f3e11
antklim <antklim@gmail.com>
2021-06-23 06:15:01 +1000

    Extra tests for default layouts

--- internal/layout/defaults_internal_test.go
+++ internal/layout/defaults_internal_test.go
@@ -14,3 +14,32 @@ func TestHttpHandler(t *testing.T) {
 	assert.Equal(t, fs.FileMode(0644), h.Permissions())
 	assert.IsType(t, &template.Template{}, h.Template())
 }
+
+func TestServiceNodes(t *testing.T) {
+	nodes := serviceNodes()
+	assert.Len(t, nodes, 6)
+	expectedNodes := []string{"adapter", "app", "handler", "provider", "server", "test"}
+	for _, n := range expectedNodes {
+		hasNode := _hasNodeWithName(nodes, n)
+		assert.True(t, hasNode)
+	}
+}
+
+func TestHttpServiceNodes(t *testing.T) {
+	nodes := httpServiceNodes()
+	assert.Len(t, nodes, 7)
+	expectedNodes := []string{"adapter", "app", "handler", "provider", "server", "test", "main.go"}
+	for _, n := range expectedNodes {
+		hasNode := _hasNodeWithName(nodes, n)
+		assert.True(t, hasNode)
+	}
+}
+
+func _hasNodeWithName(nodes []Node, name string) bool {
+	for _, n := range nodes {
+		if n.Name() == name {
+			return true
+		}
+	}
+	return false
+}
