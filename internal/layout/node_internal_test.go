659320475e9ad4077091e52532370f21c1e9d4fc
antklim <antklim@gmail.com>
2021-07-21 09:34:50 +1000

    Test restructuring

--- internal/layout/node_internal_test.go
+++ internal/layout/node_internal_test.go
@@ -3,14 +3,42 @@ package layout
 import (
 	"bytes"
 	"fmt"
-	"os"
-	"path"
 	"testing"
 	"text/template"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
+
+func TestDnode(t *testing.T) {
+	t.Run("has default directory permissions and no children when created", func(t *testing.T) {
+		n := NewDnode("test_dir")
+		expected := &Dnode{node: node{name: "test_dir", permissions: 0755}}
+		assert.Equal(t, expected, n)
+	})
+
+	t.Run("has custom directory permissions when created with permission option", func(t *testing.T) {
+		n := NewDnode("test_dir", WithDperm(0700))
+		expected := &Dnode{node: node{name: "test_dir", permissions: 0700}}
+		assert.Equal(t, expected, n)
+	})
+
+	t.Run("has non empty children list when created with children option", func(t *testing.T) {
+		f1 := NewFnode("test_file_1")
+		d1 := NewDnode("test_dir_1")
+		n := NewDnode("test_dir", WithSubNodes(f1, d1))
+		expected := &Dnode{
+			node: node{
+				name:        "test_dir",
+				permissions: 0755,
+			},
+			subnodes: []Node{
+				&Fnode{node: node{name: "test_file_1", permissions: 0644}},
+				&Dnode{node: node{name: "test_dir_1", permissions: 0755}},
+			},
+		}
+		assert.Equal(t, expected, n)
+	})
+}
 
 func TestFnode(t *testing.T) {
 	tmpl := template.Must(template.New("test").Parse("package foo"))
@@ -54,31 +82,6 @@ func TestFnode(t *testing.T) {
 	}
 }
 
-func TestFnodeBuild(t *testing.T) {
-	tmpDir := t.TempDir()
-
-	t.Run("returns an error when does not have template", func(t *testing.T) {
-		f := NewFnode("test_file_1")
-		err := f.Build(tmpDir, "module_name")
-		assert.EqualError(t, err, "node template is nil")
-
-		_, err = os.ReadFile(path.Join(tmpDir, f.Name()))
-		assert.True(t, os.IsNotExist(err))
-	})
-
-	t.Run("creates a file using node template", func(t *testing.T) {
-		f := NewFnode("test_file_2", WithNewTemplate("test", "package foo"))
-		err := f.Build(tmpDir, "module_name")
-		require.NoError(t, err)
-
-		expected := "package foo"
-
-		data, err := os.ReadFile(path.Join(tmpDir, f.Name()))
-		require.NoError(t, err)
-		assert.Equal(t, expected, string(data))
-	})
-}
-
 func TestFnodeWBuild(t *testing.T) {
 	t.Run("writes module to template", func(t *testing.T) {
 		mod := "cheftest"
@@ -92,69 +95,5 @@ import "%s/test/template"`, mod)
 		err := f.wbuild(&out, mod)
 		assert.NoError(t, err)
 		assert.Equal(t, expected, out.String())
-	})
-}
-
-func TestDnode(t *testing.T) {
-	t.Run("has default directory permissions and no children when created", func(t *testing.T) {
-		n := NewDnode("test_dir")
-		expected := &Dnode{node: node{name: "test_dir", permissions: 0755}}
-		assert.Equal(t, expected, n)
-	})
-
-	t.Run("has custom directory permissions when created with permission option", func(t *testing.T) {
-		n := NewDnode("test_dir", WithDperm(0700))
-		expected := &Dnode{node: node{name: "test_dir", permissions: 0700}}
-		assert.Equal(t, expected, n)
-	})
-
-	t.Run("has non empty children list when created with children option", func(t *testing.T) {
-		f1 := NewFnode("test_file_1")
-		d1 := NewDnode("test_dir_1")
-		n := NewDnode("test_dir", WithSubNodes(f1, d1))
-		expected := &Dnode{
-			node: node{
-				name:        "test_dir",
-				permissions: 0755,
-			},
-			subnodes: []Node{
-				&Fnode{node: node{name: "test_file_1", permissions: 0644}},
-				&Dnode{node: node{name: "test_dir_1", permissions: 0755}},
-			},
-		}
-		assert.Equal(t, expected, n)
-	})
-}
-
-func TestDnodeBuild(t *testing.T) {
-	tmpDir := t.TempDir()
-
-	t.Run("creates node directory in a provided location", func(t *testing.T) {
-		n := NewDnode("test_dir_1")
-		err := n.Build(tmpDir, "module_name")
-		require.NoError(t, err)
-
-		_, err = os.ReadDir(path.Join(tmpDir, n.Name()))
-		assert.NoError(t, err)
-	})
-
-	t.Run("creates a directory subnode", func(t *testing.T) {
-		sn := NewDnode("sub_test_dir_2")
-		n := NewDnode("test_dir_2", WithSubNodes(sn))
-		err := n.Build(tmpDir, "module_name")
-		require.NoError(t, err)
-
-		_, err = os.ReadDir(path.Join(tmpDir, n.Name(), sn.Name()))
-		require.NoError(t, err)
-	})
-
-	t.Run("creates a file subnode", func(t *testing.T) {
-		sn := NewFnode("test_file_1", WithNewTemplate("test", "package foo"))
-		n := NewDnode("test_dir_3", WithSubNodes(sn))
-		err := n.Build(tmpDir, "module_name")
-		require.NoError(t, err)
-
-		_, err = os.ReadFile(path.Join(tmpDir, n.Name(), sn.Name()))
-		require.NoError(t, err)
 	})
 }
--- internal/layout/node_test.go
+++ internal/layout/node_test.go
@@ -1,10 +1,13 @@
 package layout_test
 
 import (
+	"os"
+	"path"
 	"testing"
 
 	"github.com/antklim/chef/internal/layout"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestDnodeGetSubNode(t *testing.T) {
@@ -59,5 +62,63 @@ func TestDnodeAdd(t *testing.T) {
 
 		subnodesAfter := len(dnode.Nodes())
 		assert.Equal(t, subnodesBefore+1, subnodesAfter)
+	})
+}
+
+func TestDnodeBuild(t *testing.T) {
+	tmpDir := t.TempDir()
+
+	t.Run("creates node directory in a provided location", func(t *testing.T) {
+		n := layout.NewDnode("test_dir_1")
+		err := n.Build(tmpDir, "module_name")
+		require.NoError(t, err)
+
+		_, err = os.ReadDir(path.Join(tmpDir, n.Name()))
+		assert.NoError(t, err)
+	})
+
+	t.Run("creates a directory subnode", func(t *testing.T) {
+		sn := layout.NewDnode("sub_test_dir_2")
+		n := layout.NewDnode("test_dir_2", layout.WithSubNodes(sn))
+		err := n.Build(tmpDir, "module_name")
+		require.NoError(t, err)
+
+		_, err = os.ReadDir(path.Join(tmpDir, n.Name(), sn.Name()))
+		require.NoError(t, err)
+	})
+
+	t.Run("creates a file subnode", func(t *testing.T) {
+		sn := layout.NewFnode("test_file_1", layout.WithNewTemplate("test", "package foo"))
+		n := layout.NewDnode("test_dir_3", layout.WithSubNodes(sn))
+		err := n.Build(tmpDir, "module_name")
+		require.NoError(t, err)
+
+		_, err = os.ReadFile(path.Join(tmpDir, n.Name(), sn.Name()))
+		require.NoError(t, err)
+	})
+}
+
+func TestFnodeBuild(t *testing.T) {
+	tmpDir := t.TempDir()
+
+	t.Run("returns an error when does not have template", func(t *testing.T) {
+		f := layout.NewFnode("test_file_1")
+		err := f.Build(tmpDir, "module_name")
+		assert.EqualError(t, err, "node template is nil")
+
+		_, err = os.ReadFile(path.Join(tmpDir, f.Name()))
+		assert.True(t, os.IsNotExist(err))
+	})
+
+	t.Run("creates a file using node template", func(t *testing.T) {
+		f := layout.NewFnode("test_file_2", layout.WithNewTemplate("test", "package foo"))
+		err := f.Build(tmpDir, "module_name")
+		require.NoError(t, err)
+
+		expected := "package foo"
+
+		data, err := os.ReadFile(path.Join(tmpDir, f.Name()))
+		require.NoError(t, err)
+		assert.Equal(t, expected, string(data))
 	})
 }
