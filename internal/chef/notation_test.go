cf1dd3ff0f67eeb059fa1165ff1869ca2cfca870
antklim <antklim@gmail.com>
2022-11-16 08:46:46 +1100

    added notation write test

--- internal/chef/notation_test.go
+++ internal/chef/notation_test.go
@@ -8,23 +8,29 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-const testChefTemplate = `version: 1.0
+const testChefTemplate = `version: "1.0"
 
 project:
   name: dogs-and-cats
   description: Simple HTTP service in Go
   language: go`
 
-// func TestNotationWrite(t *testing.T) {
-// 	n := chef.Notation{Category: "srv", Server: "http"}
+func TestNotationWrite(t *testing.T) {
+	n := chef.Notation{
+		Version: "1.0",
+		Project: chef.Project{
+			Name:        "dogs-and-cats",
+			Description: "Simple HTTP service in Go",
+			Language:    "go",
+		},
+	}
 
-// 	var buf bytes.Buffer
-// 	err := n.Write(&buf)
-// 	assert.NoError(t, err)
+	var buf bytes.Buffer
+	err := n.Write(&buf)
+	assert.NoError(t, err)
 
-// 	expected := testChefTemplate
-// 	assert.YAMLEq(t, expected, buf.String())
-// }
+	assert.YAMLEq(t, testChefTemplate, buf.String())
+}
 
 func TestReadNotation(t *testing.T) {
 	var buf bytes.Buffer
